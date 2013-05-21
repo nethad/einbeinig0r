44f13e7aa4e87fe37985e827b77269e7d8ca79cd
Thomas Ritter <nethad@gmx.net>
2013-05-21 11:27:29 +0200

    add rails default files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Einbeinig0r::Application.config.secret_token = '295a2f3bc9defb04b04be3a0c5b3e98246298e1f9494cd3c45d8ac5c37f588187db08bc24590d1375833ebb7722f18c50b445075a0db4cf9c2f5db734bcf9cb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Einbeinig0r::Application.config.session_store :cookie_store, key: '_einbeinig0r_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Einbeinig0r::Application.config.session_store :active_record_store
