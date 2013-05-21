a64772fc304219b5a3f86610e46c682bfddb14ae
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2013-05-21 16:15:30 +0200

    Skype and SIP links

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,2 +1,9 @@
 module ParticipantsHelper
+  def skype_link_to(text, user)
+    link_to text, "skype:#{user}?call"
+  end
+
+  def sip_link_to(text, address)
+    link_to text, "sip:#{address}"
+  end
 end
--- test/unit/helpers/participants_helper_test.rb
+++ test/unit/helpers/participants_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class ParticipantsHelperTest < ActionView::TestCase
+  
+  should "return a valid skype link" do 
+    assert_equal '<a href="skype:foo?call">Call foo</a>', skype_link_to("Call foo", "foo")
+  end
+
+  should "return a valid sip link" do 
+    assert_equal '<a href="sip:foo">Call foo</a>', sip_link_to("Call foo", "foo")
+  end
+
 end
